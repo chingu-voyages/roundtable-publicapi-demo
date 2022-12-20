c8e01c75e42ebbff43d23fc0a19e6435f927e82a
jdmedlock <jdmedlock@gmail.com>
2022-12-20 13:10:02 -0600

    Feature: Added basic API test

---
+++ src/lib/header.svelte
@@ -0,0 +1,10 @@
+<nav class="fixed flex flex-col w-full z-10 top-0 bg-gray-900 text-white text-xl items-centers">
+  <div class="flex flex-row m-4">
+    <div>
+      <a href="https://Chingu.io" target="_blank noreferrer">
+        <img class="w-12 h-12 invisible md:visible" src="Chingu.jpg" alt="Chingu Logo"/>
+      </a>
+    </div>
+    <div class="w-1/5 ml-4 text-3xl font-bold self-center">Chingu Roundtable</div>
+  </div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Header from '$lib/header.svelte'
+
+  let searchFor = 'slither'
+
+  const validateSearchFor = (event) => {
+    
+  }
+
+  const searchForDefinition = () => {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${ searchFor }`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+</script>
+
+<Header/>
+
+<section class="pt-20 ml-6 mr-8">
+  <h2 class="mt-4 text-2xl font-bold">How to use a Public API in your web apps</h2>
+  <div class="mt-4 text-xl">
+    This app was built to provide a test platform to show how to use a public
+    API in your web apps. It utilizes the <a class="underline italic text-blue-500" target="_blank noreferrer" 
+    href="https://dictionaryapi.dev/">Free Dictionary API</a> to retrieve
+    word definitions.
+  </div>
+  <div class="mt-4 text-xl">
+    This will be referenced in an upcoming weekly Chingu Roundtable in our
+    <a class="underline italic text-blue-500" target="_blank noreferrer" 
+      href="https://www.youtube.com/playlist?list=PLmnPfbM4_HyszkLRkLtlUgoMCHer3jpg7">YouTube channel.</a>
+  </div>
+</section>
+
+<!-- Mock Checkout Form -->
+<section class="ml-6 mr-8">
+  <h2 class="mt-4 text-2xl font-bold">Mock Checkout Form</h2>
+  <div class="mt-4 ml-8 text-lg">
+    <div class="mt-4">
+      <label for="searchFor">
+        Search for the definition of:
+        <input class="border w-32" id="searchFor" name="searchFor" type="search"
+        bind:value={ searchFor } on:input={ searchFor } on:click={ validateSearchFor }>
+      </label>
+      <div class="flex flex-col items-center mt-4 w-1/6 bg-green-500 border rounded-lg">
+        <button class="font-bold" on:click={ searchForDefinition }>Checkout</button>
+      </div>
+    </div>
+  </div>
+</section>
